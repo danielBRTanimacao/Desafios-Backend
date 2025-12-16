7cf2bfeeddb1c105bd417dd45c9e32a4b89c2619
danielBRTanimacao <danieltenorio2046@gmail.com>
2025-12-16 12:31:40 -0300

    feat: add mouse event

---
+++ chess/src/main/java/daniel/event/MouseEvent.java
@@ -0,0 +1,10 @@
+package daniel.event;
+
+import java.awt.event.MouseAdapter;
+
+public class MouseEvent extends MouseAdapter {
+    @Override
+    public void mousePressed(java.awt.event.MouseEvent e) {
+        super.mousePressed(e);
+    }
+}
