db24591fc5e414c9e0531b8367c5a487f584438d
danielBRTanimacao <danieltenorio2046@gmail.com>
2026-02-20 15:30:25 -0300

    feat: creating bishop

--- chess/src/main/java/daniel/piece/Bishop.java
+++ chess/src/main/java/daniel/piece/Bishop.java
@@ -12,5 +12,17 @@ public class Bishop extends Piece {
     protected String getPieceFileName() {
         return "bishop";
     }
+
+    @Override
+    public boolean canMove(int targetCol, int targetRow) {
+        if (isWithinBoard(targetCol, targetRow) && isSameSquare(targetCol, targetRow) == false) {
+            if (Math.abs(targetCol - preCol) == Math.abs(targetRow - preRow)) {
+                if (isValidSquare(targetCol, targetRow)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
     
 }
